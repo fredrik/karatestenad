fcd9442e54c1b015c15aae58b0c62ef9c8944c5f
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2008-07-22 13:03:32 +0200

    normalization changes "?" into "questionmark", etc

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,22 +1,26 @@
 class Word < ActiveRecord::Base
-  before_create :set_normalized_word
-  
+
   validates_uniqueness_of :word
   validates_uniqueness_of :normalized_word  
+
+  before_create :set_normalized_word
+  def set_normalized_word
+    write_attribute("normalized_word", Word.normalize(word))
+  end
   
-  def self.normalize_word(w)
+  def self.normalize(word)
+    w = word.dup
+    w.gsub! '!', 'exclamationmark'
+    w.gsub! '?', 'questionmark'
+    w.gsub! ':', 'colon'
+    w.gsub! '.', 'dot'
+    w.gsub! ',', 'comma'
     # Some explanation is required: the 1st .gsub strips out anything not alphanumeric and turns it into a hyphen,
     # the 2nd .gsub changes any multiple hyphens into a single hyphen.
     # from http://www.seoonrails.com/to_param-for-better-looking-urls
-    w.gsub '!', 'exclamation_mark'
-    w.gsub '?', 'question_mark'
-    w.gsub '.', 'full_stop'
     "#{w.downcase.gsub(/[^[:alnum:]]/,'-')}".gsub(/-{2,}/,'-')
   end
 
-  def set_normalized_word
-    write_attribute("normalized_word", Word.normalize_word(word))
-  end
 
   def to_param
     normalized_word
