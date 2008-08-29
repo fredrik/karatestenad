8fc7fef59bdfb9f5c515061e9dc27b08ba936130
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2008-08-29 17:43:34 +0200

    quick refactor

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -3,6 +3,11 @@ module WordsHelper
   # splits the content of the array of stuffs into some number of arrays?
   # probably.
   def partition(stuffs)
+    half(stuffs)
+  end
+  
+  # one basic method. half the input into two basically equally sized partitions.
+  def half(stuffs)
     cut = stuffs.size/2
     a = stuffs[0..cut]
     b = stuffs[cut+1..size]
